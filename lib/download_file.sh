3e20ab5f5b00b3145f342725eb8b0e136e358f74
Karl Entwistle <me@karlentwistle.com>
2023-04-22 22:54:23 +0100

    remove --quiet

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -8,7 +8,7 @@ download_file () {
   AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
     AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
     AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
-    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $build_dir/$asset_manifest_path --quiet
+    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $build_dir/$asset_manifest_path
 
   local status=$?
 
