1b7dccb6111d497af50ec0f3d006f2eb2c9b655f
Karl Entwistle <me@karlentwistle.com>
2023-04-22 23:32:12 +0100

    tweak directory

--- lib/unpack_assets.sh
+++ lib/unpack_assets.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 unpack_assets () {
-  local assets_path=$1
+  local assets_name=$1
   local asset_manifest_path=$(get_env_variable ${env_dir} "ASSET_MANIFEST_PATH")
 
-  tar -xf $assets_path -C $asset_manifest_path
+  tar -xf $build_dir/$asset_manifest_path/$assets_name -C $build_dir/$asset_manifest_path
 }
