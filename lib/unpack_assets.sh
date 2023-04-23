88c9146360eea450ec5ced066cee7566bb534e6c
Karl Entwistle <me@karlentwistle.com>
2023-04-23 08:34:58 +0100

    delete artfact after successful extraction

--- lib/unpack_assets.sh
+++ lib/unpack_assets.sh
@@ -5,4 +5,5 @@ unpack_assets () {
   local asset_manifest_path=$(get_env_variable ${env_dir} "ASSET_MANIFEST_PATH")
 
   tar -xf $build_dir/$asset_manifest_path/$assets_name -C $build_dir/$asset_manifest_path
+  rm $build_dir/$asset_manifest_path/$assets_name
 }
